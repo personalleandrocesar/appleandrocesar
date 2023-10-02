c8921ceab560496fdbd34eedf4a8f7e23d024827
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-10-02 20:13:36 -0300

    updt

---
+++ pages/user/[id].vue
@@ -0,0 +1,11 @@
+<script setup>
+
+const route = useRoute();
+
+console.log(route.params.id);
+</script>
+<template>
+    <div>
+        Você está em: {{ $route.params.id }}
+    </div>
+</template>
