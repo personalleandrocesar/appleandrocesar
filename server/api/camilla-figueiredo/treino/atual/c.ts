42168cb88a6e435032be6919b9572dfdec81e88b
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-10-10 19:54:14 -0300

    updt

--- server/api/camilla-figueiredo/treino/atual/c.ts
+++ server/api/camilla-figueiredo/treino/atual/c.ts
@@ -18,15 +18,15 @@ export default defineEventHandler((event) => {
     //  { num: 'Exercício', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/img/preImagem.png'}
     
     // Série 1
-    { num: 'Exercício 1', nome: 'Remada no aparelho', sets: 3, reps: '8/ 12', rest: '40"', grupo: 'Quadriceps', obs: 'Fazer com a pegada neutra;' },
-    { num: 'Exercício 2', nome: 'Puxada neutra', sets: 3, reps: '12', rest: '40"', grupo: 'Quadriceps', obs: '' },
-    { num: 'Exercício 3', nome: 'Remada Curvada', sets: 3, reps: '15', rest: '40"', grupo: 'Quadriceps', obs: 'Fazer pegada pronada;' },
-    { num: 'Exercício 4', nome: 'Supino reto', sets: 3, reps: '12', rest: '40"', grupo: 'Quadriceps', obs: '' },
-    { num: 'Exercício 5', nome: 'Desenvolvimento c/ halteres', sets: 3, reps: '12', rest: '40"', grupo: 'Quadriceps', obs: '' },
-    { num: 'Exercício 6', nome: 'Tríceps Máquina', sets: 3, reps: '15', rest: '40"', grupo: 'Quadriceps', obs: '' },
-    { num: 'Exercício 7', nome: 'Remada alta', sets: 3, reps: '15', rest: '40"', grupo: 'Quadriceps', obs: 'Fazer na polia baixa;' },
-    { num: 'Exercício 8', nome: 'Tríceps corda', sets: 3, reps: '15 (c/ 1")', rest: '40"', grupo: 'Quadriceps', obs: 'Segurar 1 segundo a cada repetição' },
-    { num: 'Exercício 9', nome: 'Biceps martelo', sets: 3, reps: '12', rest: '40"', grupo: 'Quadriceps', obs: '' },
+    // { num: 'Exercício 1', nome: 'Remada no aparelho', sets: 3, reps: '8/ 12', rest: '40"', grupo: 'Quadriceps', obs: 'Fazer com a pegada neutra;' },
+    // { num: 'Exercício 2', nome: 'Puxada neutra', sets: 3, reps: '12', rest: '40"', grupo: 'Quadriceps', obs: '' },
+    // { num: 'Exercício 3', nome: 'Remada Curvada', sets: 3, reps: '15', rest: '40"', grupo: 'Quadriceps', obs: 'Fazer pegada pronada;' },
+    // { num: 'Exercício 4', nome: 'Supino reto', sets: 3, reps: '12', rest: '40"', grupo: 'Quadriceps', obs: '' },
+    // { num: 'Exercício 5', nome: 'Desenvolvimento c/ halteres', sets: 3, reps: '12', rest: '40"', grupo: 'Quadriceps', obs: '' },
+    // { num: 'Exercício 6', nome: 'Tríceps Máquina', sets: 3, reps: '15', rest: '40"', grupo: 'Quadriceps', obs: '' },
+    // { num: 'Exercício 7', nome: 'Remada alta', sets: 3, reps: '15', rest: '40"', grupo: 'Quadriceps', obs: 'Fazer na polia baixa;' },
+    // { num: 'Exercício 8', nome: 'Tríceps corda', sets: 3, reps: '15 (c/ 1")', rest: '40"', grupo: 'Quadriceps', obs: 'Segurar 1 segundo a cada repetição' },
+    // { num: 'Exercício 9', nome: 'Biceps martelo', sets: 3, reps: '12', rest: '40"', grupo: 'Quadriceps', obs: '' },
 
   ]
 })
