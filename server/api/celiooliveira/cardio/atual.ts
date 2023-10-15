28796cc901f7d3ed82f42644297bb1a32feb6281
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-10-15 17:26:57 -0300

    updt

--- server/api/celiooliveira/cardio/atual.ts
+++ server/api/celiooliveira/cardio/atual.ts
@@ -1,13 +1,13 @@
 export default defineEventHandler((event) => {
     return {
         // Treino A
-        "cardioA": "Esteira + Escada",
-        "cardioATime": "45 minutos",
-        "cardioADescription": "Esteira: 30'(caminhar forte entre 6.2 km/h a 7.0 km/h); Escada: 15'(intervalado).",
+        "cardioA": "Esteira",
+        "cardioATime": "20 minutos",
+        "cardioADescription": "Esteira: 320'(caminhar forte entre 6.2 km/h a 7.0 km/h).",
         // Treino B
-        "cardioB": "Esteira",
-        "cardioBTime": "40 minutos",
-        "cardioBDescription": "Fazer 40 minutos de forma constante, que não fique com falta de ar, no ritmo que dê para conversar com alguém, naõ ficar ofegante.",
+        "cardioB": "Bicicleta",
+        "cardioBTime": "20 minutos",
+        "cardioBDescription": "Fazer 20 minutos de forma constante, que não fique com falta de ar, no ritmo que dê para conversar com alguém, naõ ficar ofegante.",
         // Treino C
         "cardioC": "Sem cardio hoje!", 
         "cardioCTime": "--",
