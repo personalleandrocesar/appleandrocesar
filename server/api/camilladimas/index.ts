1647642d81d7904e67da2ff4d7549f8379b6af1e
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-10-20 15:02:17 -0300

    create camilladimas

---
+++ server/api/camilladimas/index.ts
@@ -0,0 +1,19 @@
+export default defineEventHandler((event) => {
+  return {
+    "nome": "Camilla",
+    "nomeCompleto": "Camilla Dimas Cotrim",
+    "email": "camilla.dimas@hotmail.com",
+    "senha": "dimas@Ca",
+    "foto": "https://m.leandrocesar.com/img/camilladimas.jpeg",
+
+    "objetivo": "Hipertrofia",
+    "tempo": "60 min.",
+    "dias": "6 dias",
+    "treinoA": "Perna",
+    "treinoB": "Braço + Abdominal",
+    "treinoC": ""
+
+    // Dados Menu Global
+    
+  }
+})
