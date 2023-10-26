867638d88d740180a224c38d061d806cf287624e
Personal Leandro Cesar <87745246+personalleandrocesar@users.noreply.github.com>
2023-10-26 05:37:48 -0300

    Atualizar o a.ts

--- server/api/luizsouza/treino/atual/a.ts
+++ server/api/luizsouza/treino/atual/a.ts
@@ -1,16 +1,17 @@
 export default defineEventHandler((event) => {
   return [
     // Série Atual
-    { num: 'Exercício 1', nome: 'Leg press horizontal', sets: 4, reps: '15', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 2', nome: 'Extensora', sets: 3, reps: '15 +15 curtas', rest: '40"', grupo: '', obs: 'Fazer 15 repetições +15 curtas(da metade para cima).', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 3', nome: 'Cadeira flexora', sets: 3, reps: '12 c/1"', rest: '40"', grupo: '', obs: 'Segurar 1 segundo a cada repetição.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 4', nome: 'Flexão de quadril', sets: 3, reps: '12', rest: '40"', grupo: '', obs: 'Fazer com caneleira.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 5', nome: 'Agachamento sumô', sets: 3, reps: '12', rest: '40"', grupo: '', obs: 'Segurar um peso bem pesado!', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 6', nome: 'Stiff', sets: 3, reps: '12', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 7', nome: 'Abdutora', sets: 3, reps: '20 2T', rest: '40"', grupo: '', obs: 'Fazer 2 tempos a cada repetição.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 8', nome: 'Adutora', sets: 3, reps: '20 c/1"', rest: '40"', grupo: '', obs: 'Segurar 1 segundo a cada repetição.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 9', nome: 'Panturrilha em pé (AP)', sets: 3, reps: '20 +isometria', rest: '40"', grupo: '', obs: 'Fazer 20 repetições e no final segurar o máximo que aguentar em cima.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
-    { num: 'Exercício 10', nome: 'Agachamento aparelho', sets: 3, reps: '12', rest: '40"', grupo: '', obs: 'Fazer de frente pro aparelho.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 1', nome: 'Puxada aberta pronada', sets: 4, reps: '15', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 2', nome: 'remada na polia baixa', sets: 3, reps: '10 c/2"', rest: '40"', grupo: '', obs: 'Fazer 10 repetições segurando 2 segundos a cada repetição).', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 3', nome: 'crucifixo inverso aparelho', sets: 3, reps: '12', rest: '40"', grupo: '', obs: 'Fazer com a pegada pronada.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 4', nome: 'Fly', sets: 3, reps: '12', rest: '40"', grupo: '', obs: 'Fazer com caneleira.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 5', nome: 'desenvolvimwnto (ap)', sets: 3, reps: '12', rest: '40"', grupo: '', obs: 'Segurar um peso bem pesado!', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 6', nome: 'supino inclinado', sets: 3, reps: '12', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 7', nome: 'abdução de ombros', sets: 3, reps: '20', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 8', nome: 'remada alta(hbm)', sets: 3, reps: '10 c/1"', rest: '40"', grupo: '', obs: 'Segurar 1 segundo a cada repetição.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 9', nome: 'Biceps em pé (hbm)', sets: 3, reps: '8 +isometria', rest: '40"', grupo: '', obs: 'Fazer 8 repetições e no final segurar o máximo que aguentar com os cotovelos em 90.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 10', nome: 'triceps corda na polia alta', sets: 3, reps: '12', rest: '40"', grupo: '', obs: 'Fazer 8 segurar 10 segundos e fazer + 8 repetições', img: 'https://m.leandrocesar.com/img/preImagem.png' },
+    { num: 'Exercício 11', nome: 'triceps mergulho no graviton', sets: 3, reps: '12', rest: '40"', grupo: '', obs: 'Fazer de frente pro aparelho.', img: 'https://m.leandrocesar.com/img/preImagem.png' },
 
 
     // Modelo:
