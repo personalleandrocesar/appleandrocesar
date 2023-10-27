7a83193074a0a518b90da53bf165925a5f998464
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-10-27 14:15:27 -0300

    updt

--- middleware/m.global.ts
+++ middleware/m.global.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
     // In a real app you would probably not redirect every route to `/`
     // however it is important to check `to.path` before redirecting or you
     // might get an infinite redirect loop
-    if (to.params.id !== 'beatrizlopes') {
-        return abortNavigation()
-    }
+    // if (to.params.id !== 'beatrizlopes') {
+    //     return abortNavigation()
+    // }
 })
