246a628fe043ca3d6a1281f1184b326d62e02105
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-10-28 20:05:59 -0300

    updt

---
+++ server/api/usernames.ts
@@ -0,0 +1,21 @@
+export default defineEventHandler((event) => {
+    return {
+    "clients": [
+        {
+            "id": 1,
+            "name": "Leandro Cesar",
+            "usuario": "leandrocesar",
+            "senha": "cesar@Le",
+            "peso": 87,
+            "img": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        },
+        {
+            "id": 2,
+            "name": "Beatriz Lopes",
+            "usuario": "beatrizlopes",
+            "senha": "lopes@Be",
+            "peso": 87,
+            "img": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        },
+    ]}
+})
--- server/api/users.ts
+++ server/api/users.ts
@@ -1,7 +1,9 @@
 export default defineEventHandler((event) => {
   return {
     "admin": {"usuario": "admin", "sennha": "1234"},
-    "camillafigueiredo": {"usuario": "camillafigueiredo", "sennha": "figueiredo@Ca"},
+    "camillafigueiredo": {
+      "usuario": "camillafigueiredo", "sennha": "figueiredo@Ca"
+    },
     "beatrizlopes": {"usuario": "beatrizlopes", "sennha": "lopes@Be"},
     "celiooliveira": {"usuario": "celiooliveira", "sennha": "oliveira@Ce"},
     "rosanaassis": {"usuario": "rosanaassis", "sennha": "assis@Ro"},
