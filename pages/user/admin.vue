b8729ae69bcb65906d68bea2374049e09bfec4c5
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-10-28 21:31:31 -0300

    updt

--- pages/user/admin.vue
+++ pages/user/admin.vue
@@ -1,10 +1,10 @@
 <script setup>
 const id = ref(1)
-const { data: product, pending, error } = await useFetch(() => `https://m.leandrocesar.com/api/usernames/${id.value}`)
+const { data: product, pending, error } = await useFetch(() => `https://dummyjson.com/products/${id.value}`)
 
 /* Same as:
 const { data: product, pending, error } = await useAsyncData(() => {
-  return $fetch(`https://m.leandrocesar.com/api/usernames/${id.value}`)
+  return $fetch(`https://dummyjson.com/products/${id.value}`)
 }, {
   watch: [id]
 })
@@ -13,7 +13,7 @@ const { data: product, pending, error } = await useAsyncData(() => {
 
 <template>
   <div>
-    <p>Result of <code>https://m.leandrocesar.com/api/usernames/</code><input type="number" v-model="id" /></p>
+    <p>Result of <code>https://dummyjson.com/products/</code><input type="number" v-model="id" /></p>
     <p><button @click="id--">Previous</button> - <button @click="id++">Next</button></p>
     <p v-if="pending">Fetching...</p>
     <pre v-else-if="error">{{ error }}</pre>
