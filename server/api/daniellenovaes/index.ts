a9552b7d2e95e097e4faaf87ec5fd64e45cf35d4
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-11-02 23:45:59 -0300

    updt

--- server/api/daniellenovaes/index.ts
+++ server/api/daniellenovaes/index.ts
@@ -3,23 +3,23 @@ export default defineEventHandler((event) => {
     "nome": "Danielle",
     "nomeCompleto": "Danielle Novaes",
     "email": "",
-    "senha": "mello@Lu",
-    "foto": "https://m.leandrocesar.com/img/daniellenovaes.jpeg",
-    "objetivo": "Perder %G.",
+    "senha": "novaes@Da",
+    "foto": "https://m.leandrocesar.com/img/daniellenovaes.jpg",
+    "objetivo": "Fortalecimento",
     "tempo": "60 min.",
-    "dias": "5-6 dias",
+    "dias": "2-4 dias",
     "treinoA": "Perna",
     "treinoB": "Braço + Abdominal",
     "treinoC": "",
 
     // Dados Menu Global
-    "periodo": "09/10/23 - 09/11/23", // Dia do pagamento Mensal
+    "periodo": "03/11/23 - 03/12/23", // Dia do pagamento Mensal
     // 0 = bloqueado; 1 = pendente; 2 = Verificado
     "status": 2,
     // Treino atual
-    "treinoActual": "26/10/23 - 26/11/23",
+    "treinoActual": "03/11/23 - 03/12/23",
     // Próxima Treino 
-    "treinoNext": "27/11/23 - 27/12/23",
+    "treinoNext": "04/12/23",
     // Avaliação atual
     "valuationActual": "?",
     // Próxima avaliação 
