73a146cca2002fa64b412325b38d1f9fd7d7fdda
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-11-13 20:18:12 -0300

    updt

--- server/api/thayanecarvalho/index.ts
+++ server/api/thayanecarvalho/index.ts
@@ -3,14 +3,14 @@ export default defineEventHandler((event) => {
 
   // Send JSON response
   return {
-    "nome": "Nira",
-    "nomeCompleto": "Alcenira Pimentel",
-    "email": "alceniralucas@gmailcom",
+    "nome": "Thayane",
+    "nomeCompleto": "Thayane Carvalho",
+    "email": "thayane.carvalhoo@gmailcom",
     "senha": "1234",
     "foto": "https://m.leandrocesar.com/img/thayanecarvalho.jpeg",
     "objetivo": "Hiṕetrofia",
     "tempo": "60 min.",
-    "dias": "3-4 dias",
+    "dias": "5 dias",
     "treinoA": "Perna",
     "treinoB": "Braço/abdomen",
     "treinoC": "",
@@ -23,8 +23,8 @@ export default defineEventHandler((event) => {
     // Próxima Treino 
     "treinoNext": "14/11/23",
     // Avaliação atual
-    "valuationActual": "10/10/23",
+    "valuationActual": "13/10/23",
     // Próxima avaliação 
-    "valuationNext": "10/01/24"
+    "valuationNext": "13/01/24"
   }
 })
