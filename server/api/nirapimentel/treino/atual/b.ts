51a983a35f7ea902f16f23f49fffbf3e7e885023
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-11-14 13:41:45 -0300

    updt

--- server/api/nirapimentel/treino/atual/b.ts
+++ server/api/nirapimentel/treino/atual/b.ts
@@ -6,7 +6,7 @@ export default defineEventHandler((event) => {
     { id: 3, num: 'Exercício 3', nome: 'Crucifixo dorsal aparelho', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/crucifixoDorsal.gif' },
     { id: 4, num: 'Exercício 4', nome: 'Remada alta na polia baixa', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada fechada.', img: 'https://m.leandrocesar.com/exe/remadaAlta.gif' },
     { id: 5, num: 'Exercício 5', nome: 'Bíceps em pé na polia baixa', sets: 3, reps: '10', rest: '50"', grupo: '', obs: 'Pegada supinada.', img: 'https://m.leandrocesar.com/exe/bicepsPolia.gif' },
-    { id: 6, num: 'Exercício 6', nome: 'Supino inclinado com halteres', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/supionInclinado.gif' },
+    { id: 6, num: 'Exercício 6', nome: 'Supino inclinado com halteres', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/supinoInclinado.gif' },
     { id: 7, num: 'Exercício 7', nome: 'Desenvolvimento de ombros (HBC)', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/desenvolvimentoHalteres.gif' },
     { id: 8, num: 'Exercício 8', nome: 'Tríceps corda na polia alta', sets: 3, reps: '12 +10" +6', rest: '50"', grupo: '', obs: 'Fazer 12 repetições, segurar 10 segundos embaixo e fazer + 6 repetiçẽos.', img: 'https://m.leandrocesar.com/exe/tricepsCorda.gif' },
     { id: 9, num: 'Exercício 9', nome: 'Tríceps máquina', sets: 3, reps: '20', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/tricepsMaquina.gif' },
