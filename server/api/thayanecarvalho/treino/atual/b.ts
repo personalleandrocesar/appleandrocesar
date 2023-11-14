fb50e6fb020995faccebcf0484ef7b41d2530492
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-11-14 15:04:24 -0300

    updt

--- server/api/thayanecarvalho/treino/atual/b.ts
+++ server/api/thayanecarvalho/treino/atual/b.ts
@@ -1,8 +1,8 @@
 export default defineEventHandler((event) => {
   return [
     // Série Atual
-    { id: 1, num: 'Exercício 1', nome: 'Puxada aberta pronada', sets: 3, reps: '12 c/1"', rest: '50"', grupo: '', obs: 'Segurar 1 segundo embaixo a cada repetição.', img: 'https://m.leandrocesar.com/exe/puxadaFrente.png' },
-    { id: 2, num: 'Exercício 2', nome: 'Remada sentada na polia baixa', sets: 3, reps: '20', rest: '50"', grupo: '', obs: 'Pegada neutra.', img: 'https://m.leandrocesar.com/exe/remadaPoliaBaixa.png' },
+    { id: 1, num: 'Exercício 1', nome: 'Puxada aberta pronada', sets: 3, reps: '12 c/1"', rest: '50"', grupo: '', obs: 'Segurar 1 segundo embaixo a cada repetição.', img: 'https://m.leandrocesar.com/exe/puxadaFrente.gif' },
+    { id: 2, num: 'Exercício 2', nome: 'Remada sentada na polia baixa', sets: 3, reps: '20', rest: '50"', grupo: '', obs: 'Pegada neutra.', img: 'https://m.leandrocesar.com/exe/remadaPoliaBaixa.gif' },
     { id: 3, num: 'Exercício 3', nome: 'Crucifixo dorsal aparelho', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/crucifixoDorsal.gif' },
     { id: 4, num: 'Exercício 4', nome: 'Supino inclinado com halteres', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/supionInclinado.gif' },
     { id: 5, num: 'Exercício 5', nome: 'Desenvolvimento de ombros (HBC)', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/desenvolvimentoHalteres.gif' },
