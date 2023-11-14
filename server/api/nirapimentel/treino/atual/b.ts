c4ac539edad36460acb20fcb9714e5d74b4f8541
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-11-14 15:18:54 -0300

    updt

--- server/api/nirapimentel/treino/atual/b.ts
+++ server/api/nirapimentel/treino/atual/b.ts
@@ -1,8 +1,8 @@
 export default defineEventHandler((event) => {
   return [
     // Série Atual
-    { id: 1, num: 'Exercício 1', nome: 'Puxada aberta pronada', sets: 3, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/puxadaFrente.png' },
-    { id: 2, num: 'Exercício 2', nome: 'Remada sentada na polia baixa', sets: 3, reps: '20', rest: '50"', grupo: '', obs: 'Pegada neutra.', img: 'https://m.leandrocesar.com/exe/remadaPoliaBaixa.png' },
+    { id: 1, num: 'Exercício 1', nome: 'Puxada aberta pronada', sets: 3, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/puxadaFrente.gif' },
+    { id: 2, num: 'Exercício 2', nome: 'Remada sentada na polia baixa', sets: 3, reps: '20', rest: '50"', grupo: '', obs: 'Pegada neutra.', img: 'https://m.leandrocesar.com/exe/remadaPoliaBaixa.gif' },
     { id: 3, num: 'Exercício 3', nome: 'Crucifixo dorsal aparelho', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/crucifixoDorsal.gif' },
     { id: 4, num: 'Exercício 4', nome: 'Remada alta na polia baixa', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada fechada.', img: 'https://m.leandrocesar.com/exe/remadaAlta.gif' },
     { id: 5, num: 'Exercício 5', nome: 'Bíceps em pé na polia baixa', sets: 3, reps: '10', rest: '50"', grupo: '', obs: 'Pegada supinada.', img: 'https://m.leandrocesar.com/exe/bicepsPolia.gif' },
