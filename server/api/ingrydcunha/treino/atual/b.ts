1a4fd7a6efa58415b74a0af7c72ecfce2ec8ca79
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-11-15 03:32:30 -0300

    updt

--- server/api/ingrydcunha/treino/atual/b.ts
+++ server/api/ingrydcunha/treino/atual/b.ts
@@ -11,7 +11,7 @@ export default defineEventHandler((event) => {
     { id: 8, num: 'Exercício 8', nome: 'Tríceps corda na polia alta', sets: 3, reps: '12 +10" +6', rest: '50"', grupo: '', obs: 'Fazer 12 repetições, segurar 10 segundos embaixo e fazer + 6 repetiçẽos.', img: 'https://m.leandrocesar.com/exe/tricepsCorda.gif' },
     { id: 9, num: 'Exercício 9', nome: 'Tríceps máquina', sets: 3, reps: '20', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/tricepsMaquina.gif' },
     { id: 10, num: 'Exercício 10', nome: 'Pull-Down', sets: 3, reps: '15', rest: '50"', grupo: '', obs: 'Fazer na polia alta com a ', img: 'https://m.leandrocesar.com/exe/pullDown.gif' },
-    { id: 11, num: 'Exercício 11', nome: 'Abdominal supra no solo', sets: 3, reps: '25', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/abdominalSupra.gif' },
+    { id: 11, num: 'Exercício 11', nome: 'Abdominal supra no solo', sets: 3, reps: '25 2T', rest: '50"', grupo: '', obs: 'Fazer 2 tempos em cima a cada repetição.', img: 'https://m.leandrocesar.com/exe/abdominalSupra.gif' },
 
     // Modelo:
     // { id: 1, num: 'Exercício 1', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/img/preImagem.png' },
