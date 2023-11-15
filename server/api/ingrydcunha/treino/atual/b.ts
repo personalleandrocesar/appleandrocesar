27751eae23d7dd3b9d28babd76450780026f3ca2
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-11-15 03:19:52 -0300

    updt

--- server/api/ingrydcunha/treino/atual/b.ts
+++ server/api/ingrydcunha/treino/atual/b.ts
@@ -1,10 +1,10 @@
 export default defineEventHandler((event) => {
   return [
     // Série Atual
-    { id: 1, num: 'Exercício 1', nome: 'Puxada aberta pronada', sets: 3, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/puxadaFrente.png' },
-    { id: 2, num: 'Exercício 2', nome: 'Remada sentada na polia baixa', sets: 3, reps: '20', rest: '50"', grupo: '', obs: 'Pegada neutra.', img: 'https://m.leandrocesar.com/exe/remadaPoliaBaixa.png' },
-    { id: 3, num: 'Exercício 3', nome: 'Crucifixo dorsal aparelho', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/crucifixoDorsal.gif' },
-    { id: 4, num: 'Exercício 4', nome: 'Remada alta na polia baixa', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada fechada.', img: 'https://m.leandrocesar.com/exe/remadaAlta.gif' },
+    { id: 1, num: 'Exercício 1', nome: 'Puxada aberta pronada', sets: 3, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/puxadaFrente.gif' },
+    { id: 2, num: 'Exercício 2', nome: 'Remada sentada (ap)', sets: 3, reps: '8 +10" +8', rest: '50"', grupo: '', obs: 'Fazer 8 repetições, segurar 10" + 8 repetições; Pegada neutra.', img: 'https://m.leandrocesar.com/exe/remadaAparelhoNeutra.gif' },
+    { id: 3, num: 'Exercício 3', nome: 'Crucifixo dorsal aparelho', sets: 3, reps: '12 c/1"', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/crucifixoDorsal.gif' },
+    { id: 4, num: 'Exercício 4', nome: 'Remada alta na polia baixa', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Segurar 1 segundo a cada repetição; Pegada fechada.', img: 'https://m.leandrocesar.com/exe/remadaAlta.gif' },
     { id: 5, num: 'Exercício 5', nome: 'Bíceps em pé na polia baixa', sets: 3, reps: '10', rest: '50"', grupo: '', obs: 'Pegada supinada.', img: 'https://m.leandrocesar.com/exe/bicepsPolia.gif' },
     { id: 6, num: 'Exercício 6', nome: 'Supino inclinado com halteres', sets: 3, reps: '12', rest: '50"', grupo: '', obs: 'Pegada pronada.', img: 'https://m.leandrocesar.com/exe/supionInclinado.gif' },
     { id: 7, num: 'Exercício 7', nome: 'Desenvolvimento de ombros (HBC)', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/desenvolvimentoHalteres.gif' },
