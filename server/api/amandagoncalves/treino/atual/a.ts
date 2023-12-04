6efc08424186a88fa93fedbc887376969064e3b8
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-12-04 15:19:31 -0300

    AM goncalves Ok

--- server/api/amandagoncalves/treino/atual/a.ts
+++ server/api/amandagoncalves/treino/atual/a.ts
@@ -10,7 +10,7 @@ export default defineEventHandler((event) => {
     { id: 7, num: 'Exercício 7', nome: 'Puxada aberta e fechada', sets: 3, reps: '12 / 7', rest: '50"', grupo: '', obs: 'Fazer 12 repetições com a pegada pronada aberta, + 7 com a pegada supinada fechada.', img: 'https://m.leandrocesar.com/exe/puxadaFrente.gif'},
     { id: 8, num: 'Exercício 8', nome: 'Remada curvada', sets: 3, reps: '15', rest: '50"', grupo: '', obs: 'Pegada Pronada Aberta.', img: 'https://m.leandrocesar.com/exe/remadacurvada.gif'},
     { id: 9, num: 'Exercício 9', nome: 'Bíceps na polia baixa', sets: 3, reps: '10 / 12', rest: '50"', grupo: '', obs: 'Fazer 10 repetições com a pegada pronada (invertida) + 12 com pegada supinada(normal).', img: 'https://m.leandrocesar.com/exe/bicepsPolia.gif'},
-    { id: 10, num: 'Exercício 10', nome: 'Abdominal remador completo no solo', sets: 3, reps: '25', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/abdominalRemador.gif'},
+    { id: 10, num: 'Exercício 10', nome: 'Abdominal remador completo no solo', sets: 3, reps: '25', rest: '50"', grupo: '', obs: 'Fazer com as duas pernas juntas.', img: 'https://m.leandrocesar.com/exe/abdominalRemador.gif'},
     { id: 11, num: 'Exercício 11', nome: 'Panturrilha em pé no aparelho', sets: 3, reps: '25 c + falha', rest: '50"', grupo: '', obs: 'Fazer 25 curtas + normal até a falha.', img: 'https://m.leandrocesar.com/exe/panturrilhaAparelho.gif'},
    
    // Modelo:
