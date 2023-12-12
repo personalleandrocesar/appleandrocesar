27911c6cb06a7ccb4fce56f2f212b1dc2984cc8c
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-12-12 08:43:55 -0300

    updt

--- server/api/ygorpaixao/cardio/atual.ts
+++ server/api/ygorpaixao/cardio/atual.ts
@@ -2,10 +2,10 @@ export default defineEventHandler((event) => {
     return {
         // Treino A
         "cardioA": "Esteira",
-        "cardioATime": "20 minutos",
+        "cardioATime": "30 minutos",
         "cardioADescription": "Caminhar forte entre 6.2 km/h a 7.0 km/h.",
         // Treino B
-        "cardioB": "Escada",
+        "cardioB": "Transport",
         "cardioBTime": "15 minutos",
         "cardioBDescription": "Fazer 15 minutos de forma constante, que não fique com falta de ar, no ritmo que dê para conversar com alguém, naõ ficar ofegante.",
         // Treino C
