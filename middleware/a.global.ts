e2d855759749f9cb4f0d0f8799a7af39a2741ad3
personalleandrocesar <leandrocesar.goo@gmail.com>
2023-12-19 19:45:14 -0300

    updt

--- middleware/a.global.ts
+++ middleware/a.global.ts
@@ -5,17 +5,44 @@ export default defineNuxtRouteMiddleware((to, from) => {
     
     logon.value = logon.value
     state.value = state.value
+    // Se o próximo id de rota for diferente de logonCookie E ao mesmo tempo o id atual
+    // if (from.params.id !== logon.value && to.params.id ) {
+        //     return navigateTo('/') 
+        // }
+        // Se o próximo id de rota for diferente de logonCookie E ao mesmo tempo o id atual
+        if (logon.value && to.fullPath === '/') {
+            return navigateTo(`/user/${logon.value}`)
+        }
+        // se o id da rota atual for diferente de logonCookie -> logonCookie será nulo!
+    //     if (from.params !== logon.value ) {
+    //         return navigateTo('/') 
+    // }    
+    // Se a rota(cheia) atual for igual a raiz E logonCookie diferente de nulo 
+    // -> stateCookie será nulo!!
+    // if (to.fullPath === '/' && logon.value !== null) {
+        //     state.value = null
+        // }
+        
+
+
+
+
+
+        
+        // Se o próximo id de rota for diferente de logonCookie E ao mesmo tempo o id atual
+            // if (from.params.id !== logon.value && to.params.id ) {
+            //     return navigateTo('/') 
+            // }
+        // se o id da rota atual for diferente de logonCookie -> logonCookie será nulo!
+            // if (to.params.id !== logon.value ) {
+            //     logon.value = null
+            // }    
+        // Se a rota(cheia) atual for igual a raiz E logonCookie diferente de nulo 
+        // -> stateCookie será nulo!!
+            // if (to.fullPath === '/' && logon.value !== null) {
+            //     state.value = null
+            // }    
+    })
     
-    if (from.params.id !== logon.value && to.params.id ) {
-        return navigateTo('/') 
-    }
+    
 
-    if (to.params.id !== logon.value ) {
-        logon.value = null
-        state.value = null
-    }    
-
-    if (to.fullPath === '/' && logon.value !== null) {
-        state.value = null
-    }    
-})
