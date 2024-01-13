1eaa690935b4a1d66c5d0252078f51aa5f678250
personalleandrocesar <leandrocesar.goo@gmail.com>
2024-01-13 00:41:54 -0300

    updt

--- components/TimelineComunity/Welcome.vue
+++ components/TimelineComunity/Welcome.vue
@@ -9,9 +9,6 @@ logon.value = reg
 const dataConf = await useFetch(`/api/${route.params.id}`)
 const status = dataConf.data.value?.status
 const photoOpen = ref(false);
-function openPhoto() {
-  photoOpen.value = !photoOpen.value;
-}
 
 // talvez não precise do código abaixo
 const logOff = () => {
@@ -25,40 +22,92 @@ tag.value = tag.value
 const bodyOne = ref(true)
 function menu() {
   bodyOne.value = !bodyOne.value
-
+  
 }
 
+const deep = ref(true)
+const mainDeep = ref(true)
+const isa = ref(false)
+
+function openDeep() {
+  deep.value = true
+  isa.value = false
+  mainDeep.value = true
+}
+function openIsa() {
+  deep.value = false
+  isa.value = true
+  mainDeep.value = false
+}
+
+const teste = useCookie('teste')
+teste.value = 0
+
+console.log(teste.value);
 </script>
 
 <template>
+  <div>
 
-    <div class="body-timeline">
-      <div class="head-logo" id="sobre">
-        <div class='logo-isa'>
-            <img @click="openPhoto()" src="~/assets/isa-arquitetura.png" alt="Foto de perfil do Personal Leandro Cesar.">
-        </div>
-        <div class='logo-deep'>
-              <img @click="openPhoto()" src="~/assets/MyLogoBold.jpg" alt="Foto de perfil do Personal Leandro Cesar.">
-          </div>
-        </div>
-        
-    </div>
+    <img @click="openDeep()" :class="{ logoDeep: deep }"   src="~/assets/MyLogoBold.jpg" alt="Foto de perfil do Personal Leandro Cesar.">
+    <img @click="openIsa()" :class="{ logoIsa: isa }" src="~/assets/isa-arquitetura.png" alt="Foto de perfil do Personal Leandro Cesar.">  
+    
+  </div>
+
+  <div class="content" v-if="mainDeep">
+    <h2>
+      Deep Acupuntura
+    </h2>
+    <h5>
+      Clínica de Terapias Chinesas
+    </h5>
+  </div>
+  <div class="content" v-else>
+    <h2>
+      Isadora Pecanha - Arquitetura
+    </h2>
+    <h5>
+      Arquitetura e interiores
+    </h5>
+  </div>
     
 </template>
 <style scoped>
+div {
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: var(--color-text);
 
-
-.head-logo {
-    display: flex;
-    justify-content: space-between;
-    flex-direction: row-reverse;
-    align-items: flex-start;
-    z-index: 1;
-    flex-wrap: wrap;
+}
+.content {
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: var(--color-text);
 
 }
 
-.logo-deep {
+img {
+  background-color: #edf2f7;
+  height: 80px;
+  width: 80px;
+  color: #718096;
+  box-shadow: 1px 7px 20px #718096;
+  margin: 1.5rem;
+  border-radius: 7px;
+  z-index: 10;
+  filter: contrast(40%);
+  transition: all .4s linear;
+}
+img:hover {
+  filter: contrast(100%);
+  box-shadow: 1px 7px 20px #434a53;
+}
+
+.logoDeep {
     display: flex;
     justify-content: center;
     flex-direction: column;
@@ -69,21 +118,17 @@ function menu() {
     width: 80px;
     color: #718096;
     box-shadow: 1px 7px 20px #ff1900;
-    margin: 1.5rem;
     border-radius: 7px;
+    filter: grayscale(0%);
     z-index: 10;
 }
-
-.logo-deep img {
-    height: 80px;
-    width: 80px;
-    border-radius: 7px;
-    border: #860d0d 2px solid;
-    z-index: 100;
-    opacity: 1;
-
-}
-.logo-isa {
+.logoDeep:hover {
+    color: #718096;
+    box-shadow: 1px 7px 20px #ff1900;
+    filter: grayscale(0%);
+  }
+  
+  .logoIsa {
     display: flex;
     justify-content: center;
     flex-direction: column;
@@ -94,109 +139,16 @@ function menu() {
     width: 80px;
     color: #718096;
     box-shadow: 1px 7px 20px #99bca6;
-    margin: 1.5rem;
     border-radius: 7px;
     z-index: 10;
-}
-
-.logo-isa img {
-    height: 80px;
-    width: 80px;
-    border-radius: 7px;
-    border: #99bca6 2px solid;
-    z-index: 100;
-    opacity: 1;
-
-}
-
-.nav-bar {
-    z-index: 200;
-    transform: translateX(0%);
-    position: fixed;
-    height: calc(100% - 0px);
-    bottom: 0px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    flex-wrap: wrap;
-}
-
-.logo-nav-bar {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    flex-wrap: wrap;
-    z-index: 100;
-    transform: translateX(0%);
-    position: fixed;
-    height: calc(100% - 0px);
-    bottom: 0px;
-    width: 100%;
-    backdrop-filter: blur(5px);
-    background-color: #ffffff50;
-
-}
-
-.logo-nav-bar img {
-    box-shadow: 1px 7px 20px #095D62;
-    height: 300px;
-    width: 300px;
-    border-radius: 7px;
-    border: #095D62 2px solid;
-    z-index: 100;
-    opacity: 1;
-
-}
-
-
-
-.body-timeline{
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  flex-wrap: wrap;
-  text-align: left;
-  margin: 2rem 10px 120px 10px;
-}
-.main-logo {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    flex-wrap: wrap;
-    background-color: #edf2f7;
-    height: 100px;
-    width: 100px;
+    filter: grayscale(0%);
+  }
+  .logoIsa:hover {
     color: #718096;
-    box-shadow: 1px 7px 20px #095D62;
-    margin: 1.5rem;
-    border-radius: 7px;
+    box-shadow: 1px 7px 20px #99bca6;
+    filter: grayscale(0%);
+    
 }
 
-.main-logo img {
-    height: 100px;
-    width: 100px;
-    border-radius: 7px;
-    border: #095D62 2px solid;
-    opacity: 1;
-
-}
-
-.body-timeline p{
-  text-align: left;
-  margin: 0 10px 20px 20px;
-  color: var(--color-text);
-}
-
-.link {
-  text-decoration: underline;
-}
-.link:hover {
-  color: #095D62;
-}
 
 </style>
