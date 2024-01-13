dc9c2e551d56c8bb884d47d67c4bbdf2d660fa77
personalleandrocesar <leandrocesar.goo@gmail.com>
2024-01-12 21:58:22 -0300

    updt

--- server/api/nailysantana/cardio/atual.ts
+++ server/api/nailysantana/cardio/atual.ts
@@ -1,13 +1,13 @@
 export default defineEventHandler((event) => {
     return {
         // Treino A
-        "cardioA": "Esteira",
-        "cardioATime": "20 minutos",
-        "cardioADescription": "Caminhar forte entre 6.2 km/h a 7.0 km/h.",
+        "cardioA": "--",
+        "cardioATime": "--",
+        "cardioADescription": "Não precisa fazer cardio neste dia!",
         // Treino B
-        "cardioB": "Escada",
-        "cardioBTime": "15 minutos",
-        "cardioBDescription": "Fazer 15 minutos de forma constante, que não fique com falta de ar, no ritmo que dê para conversar com alguém, naõ ficar ofegante.",
+        "cardioB": "Esteira",
+        "cardioBTime": "20 minutos",
+        "cardioBDescription": "Fazer 20 minutos de forma constante, que não fique com falta de ar, no ritmo que dê para conversar com alguém, naõ ficar ofegante.",
         // Treino C
         "cardioC": "", 
         "cardioCTime": "--",
