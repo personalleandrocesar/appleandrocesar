80b1722e0d1babefecdfe7af697c9c67661df91a
Personal Leandro Cesar <87745246+personalleandrocesar@users.noreply.github.com>
2024-01-31 18:13:37 +0000

    upddt

--- server/api/cristianosouza/treino/atual/a.ts
+++ server/api/cristianosouza/treino/atual/a.ts
@@ -4,10 +4,10 @@ export default defineEventHandler((event) => {
     { num: 'Exercício 1', nome: 'Supino hammer', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/supinoRetoHammer.png'},
     { num: 'Exercício 2', nome: 'Desenvolvimento hammer', sets: 3, reps: '10', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/desenvolvimentoMaquina.gif'},
     { num: 'Exercício 3', nome: 'Supino hammer inclinado', sets: 3, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/supinoHammer.gif'},
-    { num: 'Exercício 4', nome: 'Extensora', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/extensora.gif'},
+    { num: 'Exercício 4', nome: 'Extensora', sets: 4, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/extensora.gif'},
     { num: 'Exercício 5', nome: 'Adutora', sets: 3, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/adutora.gif'},
     { num: 'Exercício 6', nome: 'Tríceps barra na polia alta', sets: 3, reps: '20', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/tricepsBarra.gif'},
-    { num: 'Exercício 7', nome: 'Leg press 45º', sets: 3, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/leg45.gif'},
+    { num: 'Exercício 7', nome: 'Leg press 45º', sets: 4, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/leg45.gif'},
     { num: 'Exercício 8', nome: 'Elevação frontal', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/elevacaoFrontalHBC.gif'},
     { num: 'Exercício 9', nome: 'Abdominal aparelho', sets: 3, reps: '15', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/abdominalMaquina.gif'},
    
--- server/api/cristianosouza/treino/atual/b.ts
+++ server/api/cristianosouza/treino/atual/b.ts
@@ -7,8 +7,8 @@ export default defineEventHandler((event) => {
     { num: 'Exercício 4', nome: 'Remada Máquina', sets: 3, reps: '15', rest: '50"', grupo: '', obs: 'Fazer com a pegada pronada.', img: 'https://m.leandrocesar.com/exe/remadaMaquina.png'},
     { num: 'Exercício 5', nome: 'Remada alta na polia baixa', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/remadaAlta.gif'},
     { num: 'Exercício 6', nome: 'Bíceps barra reta na polia baixa', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/bicepsPolia.gif'},
-    { num: 'Exercício 7', nome: 'Mesa Flexora', sets: 3, reps: '10', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/mesaFlexora.gif'},
-    { num: 'Exercício 8', nome: 'Abdutora', sets: 3, reps: '20', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/abdutora.gif'},
+    { num: 'Exercício 7', nome: 'Mesa Flexora', sets: 4, reps: '10', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/mesaFlexora.gif'},
+    { num: 'Exercício 8', nome: 'Abdutora', sets: 4, reps: '20', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/abdutora.gif'},
     { num: 'Exercício 9', nome: 'Martelo', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/bicepsMartelo.gif'},
 
 
